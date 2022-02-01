33594763e915aa62b8d5cf1569377b074dc9bb5e
yashtest <yashtest@gmail.com>
2022-02-01 15:08:53 +0530

    Added Print Message

--- src/org/git/test/main/AddTest.java
+++ src/org/git/test/main/AddTest.java
@@ -5,6 +5,7 @@ public class AddTest {
 	public int add(int a,int b) {
 		if(a<0 || b<0) {
 			//Checking Conditions
+			System.out.println("Performing Addition");
 			throw new ArithmeticException("Negative Numbers Occured While Addition");
 		}
 		else
