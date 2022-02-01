979c92b8c1cbf474b33d060c33f3bc1b7085040d
yashtest <yashtest@gmail.com>
2022-02-01 15:06:23 +0530

    added perform comment

--- src/org/git/test/main/AddTest.java
+++ src/org/git/test/main/AddTest.java
@@ -4,6 +4,7 @@ public class AddTest {
 //Performing Addition
 	public int add(int a,int b) {
 		if(a<0 || b<0) {
+			//Checking Conditions
 			throw new ArithmeticException("Negative Numbers Occured While Addition");
 		}
 		else
