9e912e66a9ebfe79d4762ff68ed82701d438cbe0
yashtest <yashtest@gmail.com>
2022-02-01 13:31:53 +0530

    Added Addition To Main

--- src/org/git/test/main/GitApp.java
+++ src/org/git/test/main/GitApp.java
@@ -1,10 +1,28 @@
 package org.git.test.main;
 
+import java.util.Scanner;
+
 public class GitApp {
 
 	public static void main(String[] args) {
           //Sample Testing of Git Commands
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Testing Git Commands");
+		int a = 0 ,b = 0;
+		AddTest addTest = new AddTest();
+		try {
+			System.out.println("Enter Number 1: ");
+			a = sc.nextInt();
+			System.out.println("Enter Number 2: ");
+			 b = sc.nextInt();
+			addTest.add(a,b);
+		}catch(ArithmeticException e) {
+			System.out.println("Would You Still Like to Continue");
+			String test = sc.nextLine();
+			if(test.equalsIgnoreCase("yes") || test.equalsIgnoreCase("Yes")) {
+				addTest.add(a, b);
+			}
+		}
            
 
 	}
