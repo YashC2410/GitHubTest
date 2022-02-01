11c8adc5e2441ba4e9afb15414180b98d2dbba47
yashtest <=>
2022-02-01 11:49:58 +0530

    Initial Commit

---
+++ src/org/git/test/main/GitApp.java
@@ -0,0 +1,12 @@
+package org.git.test.main;
+
+public class GitApp {
+
+	public static void main(String[] args) {
+          //Sample Testing of Git Commands
+		System.out.println("Testing Git Commands");
+           
+
+	}
+
+}
