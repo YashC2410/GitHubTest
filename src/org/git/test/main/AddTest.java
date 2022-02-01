58733ad80ed1472ea1830b517569b11f801d95a1
yashtest <yashtest@gmail.com>
2022-02-01 15:05:03 +0530

    Addition Comment Message

--- src/org/git/test/main/AddTest.java
+++ src/org/git/test/main/AddTest.java
@@ -1,7 +1,7 @@
 package org.git.test.main;
 
 public class AddTest {
-
+//Performing Addition
 	public int add(int a,int b) {
 		if(a<0 || b<0) {
 			throw new ArithmeticException("Negative Numbers Occured While Addition");
